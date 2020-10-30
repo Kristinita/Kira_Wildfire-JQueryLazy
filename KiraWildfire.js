da82f16490ec9575725e9411c9810b0fb652d16d
Саша Черных <kristinita@cock.li>
2020-10-30 07:54:08 +0000

    Initial commit

---
+++ KiraWildfire.js
@@ -0,0 +1,17 @@
+var wildfireConfig;
+
+wildfireConfig = function() {
+	return {
+		databaseProvider: "firebase",
+		databaseConfig: {
+			apiKey: "my real API key here",
+			authDomain: "kirawildfirefirebase.firebaseapp.com",
+			databaseURL: "https://kirawildfirefirebase.firebaseio.com",
+			projectId: "kirawildfirefirebase",
+			storageBucket: "kirawildfirefirebase.appspot.com",
+			messagingSenderId: "my real sender ID here"
+		},
+		theme: "dark",
+		locale: "en"
+	};
+};
